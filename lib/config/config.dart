e7aae7eedd27b1964fda4dd8dd368a3df113d41e
sys-1015 <2520521@dwg.sen.hs.kr>
2025-10-30 03:11:18 +0900

    Update local URL for development environment

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String appName = 'TideShift';
   static const String version = '1.0.0';
-  static const String localUrl = 'http://192.168.100.206:5000'; // IMPORTANT: LOCAL WIFI IP
+  static const String localUrl = 'https://overcuriously-unawakable-kyrie.ngrok-free.dev'; // IMPORTANT: LOCAL WIFI IP
   static const String slogan =
       'Build Climate Change Friendly Habits, One Day at a Time';
 }
